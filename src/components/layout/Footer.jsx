5d363a773eb93f955594b22d40c8000f6cd5f2d6
itsMachajose <itsmachajose608@gmail.com>
2026-08-27 19:44:04 +0300

    Social Media Links

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import logo from "../../assets/logo.png"
+import logo from "../../assets/wavelinelogo.jpeg"
 
 const socialLinks = [
   { label: "TikTok", href: "https://vm.tiktok.com/ZS9BMNFmkGRne-I9wS4/" },
